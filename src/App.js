a145865ba8da9f4803314c463049b55555c698f9
Jignaciosp <nachoserrapassarini@MacBook-Pro-de-Nacho.local>
2025-09-11 12:33:37 -0300

    Me confundi antes de version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Switch } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "./Screens/Home/Home.js"
 import Series from "./Screens/Series/Series.js"
 import Peliculas from "./Screens/Peliculas/Peliculas.js"
 
 function App() {
   return (
-    <Routes>
+    <Switch>
       <Route path="/" exact={true} component={Home}/>
       <Route path="/series" component={Series}/>
       <Route path="/peliculas" component={Peliculas}/>
-    </Routes>
+    </Switch>
   );
 }
 
