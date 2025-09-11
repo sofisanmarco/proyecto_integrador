fc4db8232d1a76643f506d4bec8fedd643903c82
milagrostschudy <mtschudy@udesa.edu.ar>
2025-09-11 11:47:50 -0300

    Agregando cosas al screen de Peliculas

--- src/Screens/Peliculas/Peliculas.js
+++ src/Screens/Peliculas/Peliculas.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Movies from "../../Components/Movies/Movies";
+import Navbar from "../../Components/Navbar/Navbar";
 
 function Peliculas(){
     return(
         <React.Fragment>
+             <h1>UdeSA Movies</h1>
+             <Navbar/>
             <h2 class="alert alert-primary">Todas las películas</h2>
             <Movies/>
         </React.Fragment>
