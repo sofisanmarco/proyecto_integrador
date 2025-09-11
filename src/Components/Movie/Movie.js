507e964c7d59eb7f12172b8b3a4253c8bab043cc
milagrostschudy <mtschudy@udesa.edu.ar>
2025-09-11 11:33:22 -0300

    Subiendo Movies y Movie

---
+++ src/Components/Movie/Movie.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Movie(props){
+    <article class="single-card-movie">
+                <img src={props.img} class="card-img-top"alt={props.name}/>
+                <div class="cardBody">
+                    <h5 class="card-title">{props.name}</h5>
+                    <p class="card-text">{props.desc}</p>
+                    <a href="movie.html" class="btn btn-primary">Ver más</a>
+                </div>
+            </article>
+}
+
+export default Movie
