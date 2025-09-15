cf44acc676ddfcbf446b4ad4f87ed268e715406d
Jignaciosp <nachoserrapassarini@MacBook-Pro-de-Nacho.local>
2025-09-15 18:00:39 -0300

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,9 +4,9 @@ function Footer(){
     return(
     <footer>
     
-    <h2 className="footerNames">Sofia, Milagros, Ignacio</h2>
+    <h2 className="footerNames">® Sofia, Milagros, Ignacio</h2>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    {/* <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
@@ -14,7 +14,8 @@ function Footer(){
         crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
         integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script> */}
+        
     </footer>
     )
 }
