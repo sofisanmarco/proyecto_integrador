eefc1591e715f7bf2fa8eda29cd1613cc072a734
sofisanma <ssanmarco@udesa.edu.ar>
2025-09-20 15:52:47 -0300

    Corrigiendo errores

--- src/Components/DetalleDeSerie/DetalleDeSerie.js
+++ src/Components/DetalleDeSerie/DetalleDeSerie.js
@@ -9,16 +9,16 @@ class DetalleDeSerie extends Component{
     render(){
         return(
             <React.Fragment>
-                <h2 className="alert alert-warning">{this.props.name}</h2>
+                <h2 className="alert alert-warning">{this.props.info.name}</h2>
                 <section className="row">
                     <section className="col-md-6 info">
                         <h3>Descripción</h3>
-                        <p className="description">{this.props.desc}</p>
+                        <p className="description">{this.props.info.desc}</p>
                         <p class="mt-0 mb-0" id="release-date"><strong>Fecha de estreno:</strong> 2025-08-27</p>
                         <p class="mt-0 mb-0" id="episodes"><strong>Número de capítulos:</strong> 7</p>
                         <p class="mt-0 seasons"><strong>Temporadas:</strong> 1</p>
                     </section>
-                    <img class="col-md-6" src={this.props.img} alt=""/>
+                    <img class="col-md-6" src={this.props.info.img} alt=""/>
                 </section>
             </React.Fragment>
         )
