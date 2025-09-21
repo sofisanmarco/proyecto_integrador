c239d94d6e869544b0958e20afacab2f0e5acd81
sofisanma <ssanmarco@udesa.edu.ar>
2025-09-21 14:47:35 -0300

    Agregando cosas a detalle de Serie

--- src/Components/DetalleDeSerie/DetalleDeSerie.js
+++ src/Components/DetalleDeSerie/DetalleDeSerie.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 class DetalleDeSerie extends Component{
     constructor(props){
         super(props)
+
     }
 
     render(){
@@ -14,7 +15,9 @@ class DetalleDeSerie extends Component{
                     <section className="col-md-6 info">
                         <h3>Descripción</h3>
                         <p className="description">{this.props.detalle.overview}</p>
+                        <p class="mt-0 mb-0" id="release-date"><strong>Rating:</strong>{this.props.detalle.vote_average}</p>
                         <p class="mt-0 mb-0" id="release-date"><strong>Fecha de estreno:</strong>{this.props.detalle.first_air_date}</p>
+                        <p class="mt-0 mb-0" id="release-date"><strong>Genero:</strong>{this.props.detalle.genre}</p>
                         <p class="mt-0 mb-0" id="episodes"><strong>Número de capítulos:</strong> {this.props.detalle.number_of_episodes}</p>
                         <p class="mt-0 seasons"><strong>Temporadas:</strong> {this.props.detalle.number_of_seasons}</p>
                     </section>
