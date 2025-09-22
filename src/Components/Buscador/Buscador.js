5b29a04a0ce8148b850c248a8bfe55df0297924a
Jignaciosp <ignaciopassarini@gmail.com>
2025-09-22 05:31:27 -0300

    Search Form

--- src/Components/Buscador/Buscador.js
+++ src/Components/Buscador/Buscador.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React,{Component} from "react";
+import { withRouter } from "react-router-dom";
 
+class Buscador extends Component{
+    constructor(props) {
+        super(props);
+        this.state ={
+            valor:"",
+            tipo:"pelicula"
+        };
+    }
 
-function Buscador() {
-    return(
-        <React.Fragment>
-                
-            <form className="search-form" action="/peliculas" method="get">
-                <input type="text" name="q" placeholder="Buscar..." value=""/>
-                <button type="submit" class="btn btn-success btn-sm">Buscar</button>
+    evitarSubmit = (evento => {
+        evento.preventDefault();
+        this.props.history.push(`/resultados?query=${this.state.valor}&type=${this.state.tipo}`)
+    });
+
+    controlarCambios = (event=> {
+    this.setState({valor: event.target.value});
+    });
+
+    controlarTipo = (event=> {
+    this.setState({tipo: event.target.value});
+    });
+
+    render(){
+        return(
+            <form onSubmit={this.evitarSubmit} className="search-form">
+
+                <input
+                    type="text"
+                    className=""
+                    name="searchData"
+                    placeholder="Buscar..."
+                    onChange={this.controlarCambios}
+                    value={this.state.valor}
+                />
+
+                <select onChange={this.controlarTipo} value={this.state.tipo}>
+                    <option value="pelicula">Peliculas</option>
+                    <option value="serie">Series</option>
+                </select>
+
+                <button type="submit" className="btn btn-success btn-sm">Buscar</button>
+
             </form>
-            
-        </React.Fragment>
-    )
+        )
+    }
+
 }
-export default Buscador;
+    
+export default withRouter(Buscador);
