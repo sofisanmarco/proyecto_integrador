0fc06f5034694c096ffbfd3e0e90b51915a06131
Jignaciosp <ignaciopassarini@gmail.com>
2025-09-22 06:02:14 -0300

    Terminado Resultados.js

--- src/Screens/Resultados/Resultados.js
+++ src/Screens/Resultados/Resultados.js
@@ -11,34 +11,34 @@ class Resultados extends Component{
             tipo: "pelicula",
         };
     };
-};
 
-componentDidMount(){
-    const params = new URLSearchParams(this.props.location.search);
-    const query = params.get("query");
-    const tipo = params.get("type");
 
-    this.setState({tipo});
+    componentDidMount(){
+        const params = new URLSearchParams(this.props.location.search);
+        const query = params.get("query");
+        const tipo = params.get("type");
 
-    let endpoint="";
-    if (tipo === serie) {
-        endpoint = `https://api.themoviedb.org/3/search/tv?query=${query}&language=es-ES&page=1`;
-    } else {
-        endpoint = `https://api.themoviedb.org/3/search/movie?query=${query}&language=es-ES&page=1`;
-    };
+        this.setState({tipo});
 
-    const options = {
-        method:"GET",
-        headers:{
-            accept: "aplication/json",
-            Autorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZmUxNDZjMWIwMmUxZmI5ODZmNzEyYWM1ZDQ5NzcyYSIsIm5iZiI6MTc1Nzk2MzgzOS43MzksInN1YiI6IjY4Yzg2NjNmYmMzYjEzYWZlZGEwZjdjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GTkMeloFBEw22Du7AOtng1Dq8vMR7_XTB6txsWNiw9o"
-        },
-    };
+        let endpoint="";
+        if (tipo === "serie") {
+            endpoint = `https://api.themoviedb.org/3/search/tv?query=${query}&language=es-ES&page=1`;
+        } else {
+            endpoint = `https://api.themoviedb.org/3/search/movie?query=${query}&language=es-ES&page=1`;
+        };
 
-    fetch(endpoint, options)
-        .then((res)=>res.json())
-        .then((data) => this.setState({resultados:data.results, loading:false}))
-        .catch( error => {console.log(error); this.setState({loading:false}) });
+        const options = {
+            method:"GET",
+            headers:{
+                accept: "application/json",
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZmUxNDZjMWIwMmUxZmI5ODZmNzEyYWM1ZDQ5NzcyYSIsIm5iZiI6MTc1Nzk2MzgzOS43MzksInN1YiI6IjY4Yzg2NjNmYmMzYjEzYWZlZGEwZjdjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GTkMeloFBEw22Du7AOtng1Dq8vMR7_XTB6txsWNiw9o"
+            },
+        };
+
+        fetch(endpoint, options)
+            .then((res)=>res.json())
+            .then((data) => this.setState({resultados:data.results, loading:false}))
+            .catch( error => {console.log(error); this.setState({loading:false}) });
 
     };
 
@@ -64,4 +64,6 @@ render(){
             </section>
         </div>
     )
-}
+    }}
+
+export default Resultados;
