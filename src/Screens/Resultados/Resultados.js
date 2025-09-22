163381c1833629ccff2007b310196a5f41b8c126
Jignaciosp <ignaciopassarini@gmail.com>
2025-09-22 05:56:00 -0300

    Resultados.js

--- src/Screens/Resultados/Resultados.js
+++ src/Screens/Resultados/Resultados.js
@@ -7,14 +7,41 @@ class Resultados extends Component{
         super(props);
         this.state={
             resultados:[],
-            loading:true
+            loading:true,
+            tipo: "pelicula",
         };
-    }
-}
+    };
+};
 
 componentDidMount(){
+    const params = new URLSearchParams(this.props.location.search);
+    const query = params.get("query");
+    const tipo = params.get("type");
 
-}
+    this.setState({tipo});
+
+    let endpoint="";
+    if (tipo === serie) {
+        endpoint = `https://api.themoviedb.org/3/search/tv?query=${query}&language=es-ES&page=1`;
+    } else {
+        endpoint = `https://api.themoviedb.org/3/search/movie?query=${query}&language=es-ES&page=1`;
+    };
+
+    const options = {
+        method:"GET",
+        headers:{
+            accept: "aplication/json",
+            Autorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZmUxNDZjMWIwMmUxZmI5ODZmNzEyYWM1ZDQ5NzcyYSIsIm5iZiI6MTc1Nzk2MzgzOS43MzksInN1YiI6IjY4Yzg2NjNmYmMzYjEzYWZlZGEwZjdjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GTkMeloFBEw22Du7AOtng1Dq8vMR7_XTB6txsWNiw9o"
+        },
+    };
+
+    fetch(endpoint, options)
+        .then((res)=>res.json())
+        .then((data) => this.setState({resultados:data.results, loading:false}))
+        .catch( error => {console.log(error); this.setState({loading:false}) });
+
+    };
+
 
 render(){
 
