4d7a86bfc7b0f10337a3ae4751d4141cf8b5a940
Jignaciosp <ignaciopassarini@gmail.com>
2025-09-22 17:10:01 -0300

    Cargar mas Series.js

--- src/Components/Series/Series.js
+++ src/Components/Series/Series.js
@@ -4,7 +4,7 @@ import Serie from "../Serie/Serie";
 class Series extends Component{
     constructor (props){
     super(props);
-    this.state = {valor: "", data: [], loading:true}}
+    this.state = {valor: "", data: [], boton:"Cargar mas" ,page:1, loading:true}}
 
     componentDidMount(){
         const options = {
@@ -32,6 +32,24 @@ class Series extends Component{
     filtrarPersonajes(valor){
         return this.state.data.filter(personaje => personaje.name.toLowerCase().includes(valor.toLowerCase()) );
         }
+    
+    masSeries(){
+        const nextPage = this.state.page + 1;
+        const options = {
+            method: "GET",
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWQxNzUwYTU5YTMxZDFjZmY0MmI5NDZhYWE3MDQyZiIsIm5iZiI6MTc1ODM4MTcxMi43OTQsInN1YiI6IjY4Y2VjNjkwYzliZWIyZmZjYmQ0MTI3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0oHwn2HqoqSvfFiPbgi8EH9XlPxWosViWUbgLIVoRT0'
+            }
+        };
+
+        fetch(`https://api.themoviedb.org/3/tv/popular?language=en-US&page=${nextPage}`, options)
+        .then( response => response.json() )
+        .then( data => this.setState({data: this.state.data.concat(data.results), page: nextPage, loading:false}))
+        .catch( error => {console.log(error); this.setState({loading:false}) });;
+    }
+
+
 
         render(){
             
@@ -54,10 +72,12 @@ class Series extends Component{
                     <section className="row cards all-series" id="series">
                         {personajesFiltrados.map((item, idx) => <Serie key = {item + idx} info = {item}/>)}
                     </section>} 
+
+                        <div className="cargarMasContainer"> <button className="cargarMas" onClick={() => this.masSeries()}> Cargar mas </button> </div>
+
                 </div>
             )
         }
-    };
-
+}
 export default Series;
 
