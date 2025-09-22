b89b7adee219365f7b1ce43a0a1cec16dac09c03
Jignaciosp <ignaciopassarini@gmail.com>
2025-09-22 05:46:16 -0300

    base de resultados/js

---
+++ src/Screens/Resultados/Resultados.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import SeriePopular from "../../Components/SeriePopular/SeriePopular";
+import PopularMovie from "../../Components/PopularMovie/PopularMovie";
+
+class Resultados extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            resultados:[],
+            loading:true
+        };
+    }
+}
+
+componentDidMount(){
+
+}
+
+render(){
+
+    if (this.state.loading){
+        return <h3 className="Cargando">Cargando...</h3>
+    }
+
+
+    return(
+        <div>
+
+            <h2 className="error404" >Resultados de busqueda</h2>
+            <section className="row cards">
+
+                {this.state.resultados.map((item, idx)=>this.state.tipo==="serie" ? (<SeriePopular key={item.id + idx} info={item}/>):(<PopularMovie key={item.id + idx} info={item}/>)  )}
+
+            </section>
+        </div>
+    )
+}
