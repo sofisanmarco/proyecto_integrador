33570b7d3f9b4f098ea72b5d0c958fa0aa2ad4c2
milagrostschudy <mtschudy@udesa.edu.ar>
2025-09-22 23:06:47 -0300

    Resolviendo la parte de genero en detalle de pelicula

--- src/Components/DetalleDePelicula/DetalleDePelicula.js
+++ src/Components/DetalleDePelicula/DetalleDePelicula.js
@@ -5,6 +5,7 @@ class DetalleDePelicula extends Component{
     constructor(props){
         super(props)
         this.state = {favoritos: false}
+        genres = []
     }
 
     agregarFavoritos(){
@@ -59,7 +60,7 @@ class DetalleDePelicula extends Component{
                     <p className="mt-0 mb-0" id="release-date"><strong>Release Date:</strong> {this.props.detalle.release_date}</p>
                     <p className="mt-0 mb-0 length"><strong>Runtime:</strong> {this.props.detalle.runtime}</p>
                     <p className="mt-0" id="votes"><strong>Rating:</strong> {this.props.detalle.vote_average}</p>
-                    <p className="mt-0" id="votes"><strong>Genre:</strong> {this.props.detalle.genres.map(item => item.name)}</p>
+                    <p className="mt-0" id="votes"><strong>Genre:</strong> {this.props.detalle.genre}</p>
                     
                     {this.state.favoritos ? <div ><button className="btn alert-primary" onClick={() => this.quitarDeFavoritos()} >♥️</button></div>
                     : <div><button className="btn alert-primary" onClick={() => this.agregarFavoritos()} >🩶</button></div>}
